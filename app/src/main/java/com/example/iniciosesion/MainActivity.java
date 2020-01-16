4778774fde6bcd00d5104914d05502e83a93430f
Javier Arce <jasearce@espol.edu.ec>
2020-01-15 20:57:37 -0500

    pantalla principal

---
+++ app/src/main/java/com/example/iniciosesion/MainActivity.java
@@ -0,0 +1,38 @@
+package com.example.iniciosesion;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button btnUsuario, btnGuardia;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        btnUsuario = findViewById(R.id.btnUser);
+        btnGuardia = findViewById(R.id.btnGuardia);
+    }
+
+    public void onClick(View view){
+        Intent intent;
+        switch (view.getId()){
+            case R.id.btnUser:
+                intent = new Intent(MainActivity.this, LoginUserActivity.class);
+                startActivity(intent);
+                finish();
+                break;
+            case R.id.btnGuardia:
+                intent = new Intent(MainActivity.this, LoginGuardiaActivity.class);
+                startActivity(intent);
+                finish();
+                break;
+        }
+    }
+}
